67e00c29634b9fa86539721bb4d30d3f5ed77554
tanjumtanha <tanha.tanni27@gmail.com>
2023-05-04 08:18:51 +0600

    data file for chef kitchenary site

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+const data = require('./data/data.json')
+
+app.use(cors());
+app.get('/', (req,res)=>{
+    res.send('Recipe Book is running')
+})
+
+app.get('/data', (req,res)=>{
+    console.log(data)
+    res.send(data)
+})
+
+app.get('/data/:id',(req,res)=>{
+    const id = req.params.id;
+    const  selectedChef= data.find(food=> food.id == id)
+    res.send(selectedChef);
+})
+
+app.listen(port, ()=>{
+    console.log(`Recipe book api is running on port: ${port}`)
+})
